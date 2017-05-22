109d75f2f8bf7ee3aee98ff27b21828815f659a3
Timothy Place <tim@cycling74.com>
2017-05-22 14:16:31 -0500

    cleanup, cuts, and updates to onepole filter and sync oscillator

---
+++ include/c74_lib_onepole.h
@@ -0,0 +1,56 @@
+/// @file
+///	@ingroup 	minlib
+/// @author		Timothy Place, Nathan Wolek
+///	@copyright	Copyright (c) 2017, Cycling '74
+///	@license	Usage of this file and its contents is governed by the MIT License
+
+#pragma once
+
+
+namespace c74 {
+namespace min {
+namespace lib {
+
+
+	///	Single-channel, basic <a href="https://en.wikipedia.org/wiki/Low-pass_filter">low-pass filter</a>.
+	///	Handy for use in smoothing control signals or damping high frequency components.
+
+	class onepole {
+	public:
+
+		/// Set filter coefficient directly.
+
+		void coefficient(number new_coefficient) {
+			new_coefficient = MIN_CLAMP(new_coefficient, 0.0, 1.0);
+			a_0 = new_coefficient;
+			b_1 = 1 - new_coefficient;
+		}
+
+		/// Set filter coefficient using a cutoff frequency.
+		///	@see http://musicdsp.org/showArchiveComment.php?ArchiveID=237
+
+		void frequency(number cutoff_frequency, number sampling_frequency) {
+			coefficient(1.0 - exp(-2.0 * M_PI * cutoff_frequency / sampling_frequency));
+		}
+
+
+		void clear() {
+			y_1 = 0.0;
+		}
+
+
+		sample operator()(sample x) {
+			auto y = (x * a_0) + (y_1 * b_1);
+			y_1 = y;
+			return y;
+		}
+
+	private:
+		number a_0 { 0.5 };	///< gain coefficient
+		number b_1 { 0.5 };	///< feedback coefficient
+		sample y_1 {};		///< previous output sample
+	};
+
+
+}}}  // namespace c74::min::lib
+
--- include/c74_lib_sync.h
+++ include/c74_lib_sync.h
@@ -6,97 +6,66 @@
 
 #pragma once
 
-#include "../core/JamomaAudioObject.h"
-
 
 namespace c74 {
 namespace min {
+namespace lib {
 
+	///	Generate a non-bandlimited <a href="https://en.wikipedia.org/wiki/Sawtooth_wave">sawtooth wave</a> oscillator (a phasor~ in MSP parlance).
+	/// This function is typically used as a control signal for <a href="https://en.wikipedia.org/wiki/Phase_(waves)">phase</a> ramping.
 
-	/**	This AudioObject generates a non-bandlimited <a href="https://en.wikipedia.org/wiki/Sawtooth_wave">sawtooth wave</a> oscillator.
-        This function is typically used as a control signal for <a href="https://en.wikipedia.org/wiki/Phase_(waves)">phase</a> ramping.
-	 */
-	class Sync : public AudioObject {
-		double	mPhase = 0.0;
-		double	mStep = 0.0;
+	class sync {
+	public:
 
-		void setStep()
-		{
-            mStep = frequency / sampleRate;
-			ZeroDenormal(mStep);
+		void frequency(number oscillator_frequency, number sampling_frequency) {
+			oscillator_frequency = MIN_CLAMP(oscillator_frequency, sampling_frequency * -0.5, sampling_frequency * 0.5),
+			m_step = oscillator_frequency / sampling_frequency;
+
 		}
 
-
-	public:
-		static constexpr Classname classname = { "sync" };
-		static constexpr auto tags = { "dspGeneratorLib", "audio", "generator", "oscillator" };
-
-
-		Parameter<double, Limit::Wrap<double>, NativeUnit::None<double>>	phase		= { this,
-																						"phase",
-																						0.0,
-																						Range<double>(0.0, 1.0),
-																						Setter([this]{
-																							mPhase = phase;
-																						})
-		};
+		//number frequency() {
+		//	return m_frequency;
+		//}
 
 
-		Parameter<double, Limit::Fold<double>, NativeUnit::None<double>>	frequency	= {	this,
-																						"frequency",
-																						1.0,
-																						Range<double>(sampleRate * -0.5, sampleRate * 0.5),
-																						Setter( [this]{
-																							setStep();
-																						} ),
-																						Synopsis("Rate at which to cycle")
-		};
+		void phase(number new_phase) {
+			m_phase = MIN_CLAMP(new_phase, 0.0, 1.0);
+		}
 
-
-		Parameter<double, Limit::None<double>, NativeUnit::LinearGain>		gain = { this, "gain", 1.0 };		///< scaling applied to the output
-
-
-		Parameter<double>													offset = { this, "offset", 0.0 };	///< shift applied to the output
-
-
+		number phase() {
+			return m_phase;
+		}
 
 
 		// TODO: add offset Parameter from Jamoma1
 		// TODO: add gain Parameter from Jamoma1
 
 
-		/** Process one sample.
-			@param	x	Sample to be processed.
-			@return		Processed sample
-		 */
-		Sample operator()(const Sample x)
-		{
-			if (mPhase > 1.0)
-				mPhase -= 1.0;
-			else if (mPhase < 0.0)
-				mPhase += 1.0;
+		/// Process one sample.
+		///	@param	x	Sample to be processed.
+		///	@return		Processed sample
 
-			Sample y = mPhase * gain + offset;
+		sample operator()(sample x) {
+			if (m_phase > 1.0)
+				m_phase -= 1.0;
+			else if (m_phase < 0.0)
+				m_phase += 1.0;
 
-			mPhase += mStep;
+			auto y = m_phase * m_gain + m_offset;
+
+			m_phase += m_step;
 			return y;
 		}
 
 
-		/** Process a SharedSampleBundleGroup.
-			@param	x	SharedSampleBundleGroup to be processed.
-			@return		Processed SharedSampleBundleGroup.
-		 */
-		SharedSampleBundleGroup operator()(const SampleBundle& x = kSampleBundleNone)
-		{
-			auto out = mOutput;
+	private:
 
-			for (int channel=0; channel<x.channelCount(); ++channel)
-                for	(auto& sample : out[0][channel])
-                    sample = (*this)(0.0);
-			return out;
-		}
+		number	m_gain		{ 1.0 };
+		number	m_offset	{};
+		number	m_phase		{};
+		number	m_step		{};
 
 	};
 
-}}  // namespace c74::min
+
+}}}  // namespace c74::min::lib
