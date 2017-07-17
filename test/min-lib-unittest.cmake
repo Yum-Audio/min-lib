f41da1023458a882c3aa91cc031b300446c4c92a
Timothy Place <tim@cycling74.com>
2017-07-17 09:03:24 -0600

    retooling and enabling unit testing

---
+++ test/min-lib-unittest.cmake
@@ -0,0 +1,40 @@
+cmake_minimum_required(VERSION 3.0)
+
+project(${PROJECT_NAME}_test)
+
+enable_testing()
+
+include_directories( 
+	"${C74_INCLUDES}"
+	"${CMAKE_CURRENT_LIST_DIR}/../../min-api/test/catch/include"
+#	"${CMAKE_CURRENT_LIST_DIR}/mock"
+)
+
+#set(CMAKE_CXX_FLAGS "-std=c++1y -stdlib=libc++ -fprofile-arcs -ftest-coverage")
+#set(CMAKE_CXX_FLAGS "-fprofile-arcs -ftest-coverage")
+#set(CMAKE_C_FLAGS "-fprofile-arcs -ftest-coverage")
+#SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fprofile-arcs -ftest-coverage")
+
+if (APPLE)
+	set(CMAKE_OSX_ARCHITECTURES x86_64;i386)
+endif ()
+
+add_executable(${PROJECT_NAME} ${PROJECT_NAME}.cpp)
+
+target_link_libraries(${PROJECT_NAME} "mock_kernel")
+
+if (APPLE)
+	target_link_libraries(${PROJECT_NAME} "-weak_framework JitterAPI")
+	set_target_properties(${PROJECT_NAME} PROPERTIES LINK_FLAGS "-Wl,-F'${C74_MAX_API_DIR}/lib/mac'")
+endif ()
+if (WIN32)
+	add_custom_command(	TARGET ${PROJECT_NAME} 
+						POST_BUILD	# Adds a post-build event to MyTest
+    					COMMAND ${CMAKE_COMMAND} -E copy_if_different		# which executes "cmake - E copy_if_different..."
+        				"${CMAKE_CURRENT_SOURCE_DIR}/../../../../tests/mock_kernel.dll"	# <--this is in-file
+       				 	${CMAKE_CURRENT_BINARY_DIR}                 					# <--this is out-file path
+)
+endif ()
+
+add_test(NAME ${PROJECT_NAME}
+         COMMAND ${PROJECT_NAME})
---
+++ test/sync/sync_test.cpp
@@ -0,0 +1,67 @@
+///	@brief 		Unit test for the Sync class
+///	@author		Timothy Place, Nathan Wolek
+///	@copyright	Copyright (c) 2005-2015 The Jamoma Group, http://jamoma.org.
+///	@license	This project is released under the terms of the MIT License.
+
+//#include "c74_min_unittest.h"		// required unit test header
+//#include "min.phasor_tilde.cpp"		// need the source of our object so that we can access it
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "c74_min.h"
+
+
+SCENARIO ("responds appropriately to messages and attrs") {
+
+	GIVEN ("An instance of the sync class") {
+		c74::min::lib::sync	s;
+
+		REQUIRE( s.phase() == 0.0 );
+
+		WHEN ("phase is set within a valid range") {
+			s.phase(0.25);
+			THEN("phase is set to the value specified");
+			REQUIRE( s.phase() == Approx(0.25) );
+		}
+		AND_WHEN ("setting phase param over range") {
+			s.phase(1.3);
+			THEN("phase is wrapped into range (integral part is discarded)" );
+			REQUIRE( s.phase() == Approx(0.3) );
+		}
+		AND_WHEN ("setting phase param way over range") {
+			s.phase(2.45);
+			THEN("phase is wrapped into range (integral part is discarded)" );
+			REQUIRE( s.phase() == Approx(0.45) );
+		}
+		AND_WHEN ("setting phase param under range") {
+			s.phase(-1.3);
+			THEN( "phase is wrapped into range" );
+			REQUIRE( s.phase() == Approx(0.7) );
+		}
+
+		// There is no gain or offset parameter because we are calculating single-sample and
+		// this class is kept to its essential function
+
+		WHEN ("frequency is set within a valid range") {
+			s.frequency(1000.0, 96000.0);
+			THEN("frequency is set to the value specified");
+			REQUIRE( s.frequency() == Approx(1000.0) );
+		}
+		AND_WHEN ("setting frequency param way above range 1") {
+			s.frequency(50000.0, 96000.0);
+			THEN("frequency is folded back down into range");
+			REQUIRE( s.frequency() == Approx(46000.0) );
+		}
+		AND_WHEN ("setting frequency param way above range 2") {
+			s.frequency(98000.0, 96000.0);
+			THEN("frequency is folded back down into range");
+			REQUIRE( s.frequency() == Approx(-2000.0) );
+		}
+		AND_WHEN ("setting frequency param below range") {
+			s.frequency(-2000.0, 96000.0);
+			THEN("frequency is set to the value specified");
+			REQUIRE( s.frequency() == Approx(-2000.0) );
+		}
+	}
+}
+
