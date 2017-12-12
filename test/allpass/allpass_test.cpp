d69ab55901371bf1940b7413870bdf8414e03ec2
Nathan Wolek <nathanwolek@LowkeyMacBook.local>
2017-12-12 18:20:04 -0500

    allpass: adding new test with delay set to 2 samples, 0.75 gain. PASSES. see Cycling74/min-lib/issue #17

--- test/allpass/allpass_test.cpp
+++ test/allpass/allpass_test.cpp
@@ -352,3 +352,106 @@ TEST_CASE ("Produce the correct impulse response for a delay setting of 4 sample
     REQUIRE_VECTOR_APPROX(output, reference);
 }
 
+TEST_CASE ("Produce the correct impulse response for a delay setting of 2 samples") {
+    
+    using namespace c74::min;
+    using namespace c74::min::lib;
+    INFO ("Using an allpass instance with arguments, which should set default gain at 0.0 and new capacity to 2 samples");
+    
+    allpass	f { 2 };
+    REQUIRE( f.gain() == 0.0 );		// check the default value
+    REQUIRE( f.delay() == 2 );     // check the initialized value
+    
+    INFO("Changing the gain to 0.75...");
+    f.gain(0.75);
+    REQUIRE( f.gain() == 0.75 );		// check the new value
+    
+    INFO ("And then pushing a 64-sample impulse through the unit...");
+    sample_vector impulse(64, 0.0);
+    impulse[0] = 1.0;
+    
+    // output from our object's processing
+    sample_vector	output;
+    
+    // run the calculations
+    for (auto x : impulse) {
+        auto y = f(x);
+        output.push_back(y);
+    }
+    
+    INFO ("And finally checking our output against a reference prodcued with Octave.");
+    // coefficients calculated in Octave
+    //	a3 = [0.75, 0.0, 1.0];	% numerator (fir)
+    //	b3 = [1.0, 0.0, 0.75];	% denominator (iir)
+    //	i = impz(a, b, 64);
+    
+    sample_vector reference = {
+        0.75,
+        0,
+        0.4375,
+        0,
+        -0.328125,
+        0,
+        0.24609375,
+        0,
+        -0.1845703125,
+        0,
+        0.138427734375,
+        0,
+        -0.10382080078125,
+        0,
+        0.0778656005859375,
+        0,
+        -0.05839920043945312,
+        0,
+        0.04379940032958984,
+        0,
+        -0.03284955024719238,
+        0,
+        0.02463716268539429,
+        0,
+        -0.01847787201404572,
+        0,
+        0.01385840401053429,
+        0,
+        -0.01039380300790071,
+        0,
+        0.007795352255925536,
+        0,
+        -0.005846514191944152,
+        0,
+        0.004384885643958114,
+        0,
+        -0.003288664232968586,
+        0,
+        0.002466498174726439,
+        0,
+        -0.001849873631044829,
+        0, 
+        0.001387405223283622, 
+        0, 
+        -0.001040553917462717, 
+        0, 
+        0.0007804154380970374, 
+        0, 
+        -0.000585311578572778, 
+        0, 
+        0.0004389836839295835, 
+        0, 
+        -0.0003292377629471877, 
+        0, 
+        0.0002469283222103907, 
+        0, 
+        -0.0001851962416577931, 
+        0, 
+        0.0001388971812433448, 
+        0, 
+        -0.0001041728859325086, 
+        0, 
+        7.812966444938144e-05, 
+        0
+    };
+    
+    // check it
+    REQUIRE_VECTOR_APPROX(output, reference);
+}
