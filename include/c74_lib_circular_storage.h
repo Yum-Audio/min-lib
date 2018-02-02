64cbeaa723e8650fc0914ec8ee43dac1389c1f66
Timothy Place <tim@cycling74.com>
2018-02-02 08:29:14 -0700

    Revert "circular storage: fix for crashes when the amount of wrapping to be done to get index back into range is unexpectedly large. This can happen, for example, if there is a dramatic down-sizing of the active storage slots. see nwolek/min-verbs#5."
    This reverts commit 2160ec591d48ddad3cc18aa3132b6bdfb84b1990.

--- include/c74_lib_circular_storage.h
+++ include/c74_lib_circular_storage.h
@@ -110,7 +110,7 @@ namespace lib {
 			auto start = static_cast<long>( m_index - count );
 			bool wrap = false;
 
-			while (start<0) {
+			if (start<0) {
 				count = -start;
 				start = static_cast<long>(size()) + start;
 				wrap = true;
@@ -142,7 +142,7 @@ namespace lib {
 			auto start = static_cast<long>( m_index % size() );
 			bool wrap = false;
 
-			while (start+count > size()) {
+			if (start+count > size()) {
 				count = static_cast<long>(size()) - start;
 				wrap = true;
 			}
