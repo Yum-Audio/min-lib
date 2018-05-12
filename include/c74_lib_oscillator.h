0a627f7c1506e394ab3484c42a60071dc86dc929
Nathan Wolek <nathanwolek@LowkeyMacBook.local>
2018-05-12 14:40:22 -0400

    oscillator: establishing new file for class. see Cycling74/min-lib/issues/30

---
+++ include/c74_lib_oscillator.h
@@ -0,0 +1,21 @@
+/// @file
+///	@ingroup 	minlib
+///	@copyright	Copyright 2018 The Min-Lib Authors. All rights reserved.
+///	@license	Use of this source code is governed by the MIT License found in the License.md file.
+
+#pragma once
+
+namespace c74 {
+namespace min {
+namespace lib {
+	
+	/// Generate basic <a href="https://en.wikipedia.org/wiki/Waveform">waveforms</a> using a single-cycle <a href="https://en.wikipedia.org/wiki/Wavetable_synthesis">wavetable</a>
+	
+	class oscillator {
+	public:
+		
+	private:
+		
+	};
+	
+}}}  // namespace c74::min::lib
