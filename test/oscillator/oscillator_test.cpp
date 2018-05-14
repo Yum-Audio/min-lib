5d57a90e250aae465897632042a4bd8a5c79d92d
Nathan Wolek <nathanwolek@LowkeyMacBook.local>
2018-05-13 21:26:25 -0400

    oscillator: test for the sawtooth_unipolar waveform PASSES. see Cycling74/min-lib/issues/30

--- test/oscillator/oscillator_test.cpp
+++ test/oscillator/oscillator_test.cpp
@@ -506,3 +506,98 @@ TEST_CASE ("Produce a unipolar ramp waveform") {
 	
 	
 }
+
+
+TEST_CASE ("Produce a unipolar sawtooth waveform") {
+	
+	using namespace c74::min;
+	using namespace c74::min::lib;
+	INFO ("Using an oscillator instance with wavetable size of 64, setting waveform to unipolar sawtooth");
+	
+	oscillator<> o { 64 };
+	o.change_waveform<generator::sawtooth_unipolar<>>();
+	
+	INFO ("Setting frequency to produce single cycle with duration of 64 samples");
+	o.frequency(1.0, 64.0);
+	
+	c74::min::sample_vector reference = {
+		0,
+		0.015625,
+		0.03125,
+		0.046875,
+		0.0625,
+		0.078125,
+		0.09375,
+		0.109375,
+		0.125,
+		0.140625,
+		0.15625,
+		0.171875,
+		0.1875,
+		0.203125,
+		0.21875,
+		0.234375,
+		0.25,
+		0.265625,
+		0.28125,
+		0.296875,
+		0.3125,
+		0.328125,
+		0.34375,
+		0.359375,
+		0.375,
+		0.390625,
+		0.40625,
+		0.421875,
+		0.4375,
+		0.453125,
+		0.46875,
+		0.484375,
+		0.5,
+		0.515625,
+		0.53125,
+		0.546875,
+		0.5625,
+		0.578125,
+		0.59375,
+		0.609375,
+		0.625,
+		0.640625,
+		0.65625,
+		0.671875,
+		0.6875,
+		0.703125,
+		0.71875,
+		0.734375,
+		0.75,
+		0.765625,
+		0.78125,
+		0.796875,
+		0.8125,
+		0.828125,
+		0.84375,
+		0.859375,
+		0.875,
+		0.890625,
+		0.90625,
+		0.921875,
+		0.9375,
+		0.953125,
+		0.96875,
+		0.984375
+	};
+	
+	// output from our object's processing
+	sample_vector	output;
+	
+	// run the calculations
+	for (auto x : reference) {
+		auto y = o();
+		output.push_back(y);
+	}
+	
+	INFO("The output matches an externally generated reference set.");
+	REQUIRE_VECTOR_APPROX( output, reference );
+	
+	
+}
